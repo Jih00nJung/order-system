0baee02ffe13769f9124ecd6a7c4bc00edd9610e
jih0on <wjdwlgns11q@gmail.com>
2026-06-22 23:03:19 +0900

    refactor(order): FETCH JOIN 추가 및 Repository 메서드 추가

--- src/main/java/io/github/jih0on/ordersystem/order/repository/OrderRepository.java
+++ src/main/java/io/github/jih0on/ordersystem/order/repository/OrderRepository.java
@@ -2,7 +2,15 @@ package io.github.jih0on.ordersystem.order.repository;
 
 import io.github.jih0on.ordersystem.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    // 주문 조회
+    // FETCH JOIN
+    @Query("SELECT DISTINCT o FROM Order o JOIN FETCH o.orderItems WHERE o.orderId IN :orderIds")
+    List<Order> findOrderAndItemsById(@Param("orderIds") List<Long> orderIds);
 }
--- src/main/java/io/github/jih0on/ordersystem/payment/repository/PaymentRepository.java
+++ src/main/java/io/github/jih0on/ordersystem/payment/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package io.github.jih0on.ordersystem.payment.repository;
 import io.github.jih0on.ordersystem.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrder_OrderId(Long orderId);
 }
